aeae8ea5b5b1e2392ec86c02e172d955eec52b27
Mariusz Litwin <litwin@oposec.pl>
2019-06-16 01:44:38 +0200

    [rebuild.pl] Reformat script

--- rebuild.pl
+++ rebuild.pl
@@ -9,98 +9,97 @@ use File::Basename;
 use Parse::CPAN::Meta;
 
 sub flatten {
-    my $hash = shift;
-    my $prefix = shift // '';
-    my %flathash;
+  my $hash = shift;
+  my $prefix = shift // '';
+  my %flathash;
 
-    # add some dynamic entries to flathash
+  # add some dynamic entries to flathash
+  $flathash{'system.dotfilespath'} = "$::scriptdir";
+  $flathash{'system.utilspath'} = "$::scriptdir/build/run-on-demand";
 
-    $flathash{'system.dotfilespath'} = "$::scriptdir";
-    $flathash{'system.utilspath'} = "$::scriptdir/build/run-on-demand";
+  foreach (keys %{$hash}) {
+    my @cprefix = grep(length, ($prefix, $_));
 
-    foreach (keys %{$hash}) {
-        my @cprefix = grep(length, ($prefix, $_));
-
-        if (ref($hash->{$_}) eq "HASH") {
-            %flathash = (%flathash, flatten($hash->{$_}, $_));
-        } elsif (ref($hash->{$_}) eq "ARRAY") {
-            while (my ($i, $v) = each(@{$hash->{$_}})) {
-                $flathash{join('.', (@cprefix, $i))} = $v;
-            }
-        } else {
-            $flathash{join('.', @cprefix)} = $hash->{$_};
-        }
+    if (ref($hash->{$_}) eq "HASH") {
+      %flathash = (%flathash, flatten($hash->{$_}, $_));
+    } elsif (ref($hash->{$_}) eq "ARRAY") {
+      while (my ($i, $v) = each(@{$hash->{$_}})) {
+        $flathash{join('.', (@cprefix, $i))} = $v;
+      }
+    } else {
+      $flathash{join('.', @cprefix)} = $hash->{$_};
     }
+  }
 
-    return %flathash;
+  return %flathash;
 }
 
 sub template {
-    my ($text, $vars) = @_;
+  my ($text, $vars) = @_;
 
-    foreach (keys %{$vars}) {
-        my $find = quotemeta "<% $_ %>";
-        my $replace = $vars->{$_};
-            $text =~ s/$find/$replace/g
-    }
-    return $text;
+  foreach (keys %{$vars}) {
+    my $find = quotemeta "<% $_ %>";
+    my $replace = $vars->{$_};
+    $text =~ s/$find/$replace/g
+  }
+  return $text;
 }
 
 sub dirwalk {
-    my $dirname = shift;
-    my $callback = shift;
-    my $skiphidden = shift // 0;
+  my $dirname = shift;
+  my $callback = shift;
+  my $skiphidden = shift // 0;
 
-    opendir(my $dh, $dirname) or die "[ERROR] Can't open $dirname: $!";
-    foreach $_ (sort readdir $dh) {
-        unless ($_ eq "." or $_ eq "..") {
-            my $cpath = "$dirname/$_";
-            if (-d $cpath) {
-                print "[INFO] Going down to $cpath directory\n";
-                $callback->($cpath);
-                dirwalk($cpath, $callback); 
-            } else {
-                unless ($skiphidden and substr($_, 0, 1) eq '.') {
-                    print "[INFO] Processing $cpath file\n";
-                    $callback->($cpath);
-                }
-            }
+  opendir(my $dh, $dirname) or die "[ERROR] Can't open $dirname: $!";
+  foreach $_ (sort readdir $dh) {
+    unless ($_ eq "." or $_ eq "..") {
+      my $cpath = "$dirname/$_";
+      if (-d $cpath) {
+        print "[INFO] Going down to $cpath directory\n";
+        $callback->($cpath);
+        dirwalk($cpath, $callback); 
+      } else {
+        unless ($skiphidden and substr($_, 0, 1) eq '.') {
+          print "[INFO] Processing $cpath file\n";
+          $callback->($cpath);
         }
+      }
     }
-    closedir $dh;
+  }
+  closedir $dh;
 }
 
 sub build {
-    my ($flatconfig, $tdir, $bdir, $path) = @_;
+  my ($flatconfig, $tdir, $bdir, $path) = @_;
 
-    if (-d $path) {
-        $path =~ s/$tdir/$bdir/;
-        mkdir $path or die("[ERROR] Failed to create directory $path: $!");
-    } else {
-        open(my $ifh, "<:encoding(UTF-8)", $path)
-            or die("[ERROR] Failed to open file $path: $!");
+  if (-d $path) {
+    $path =~ s/$tdir/$bdir/;
+    mkdir $path or die("[ERROR] Failed to create directory $path: $!");
+  } else {
+    open(my $ifh, "<:encoding(UTF-8)", $path)
+      or die("[ERROR] Failed to open file $path: $!");
 
-        $path =~ s/$tdir/$bdir/;
-        open(my $ofh, ">:encoding(UTF-8)", $path)
-            or die ("[ERROR] Failed to open file $path: $!");
+    $path =~ s/$tdir/$bdir/;
+    open(my $ofh, ">:encoding(UTF-8)", $path)
+      or die ("[ERROR] Failed to open file $path: $!");
 
-        print $ofh map { template($_, $flatconfig) } <$ifh>;
-        close($ifh) or warn("[WARNING] Failed to close file $path: $!");
-        close($ofh) or warn("[WARNING] Failed to close file $path, $!");
-    }
+    print $ofh map { template($_, $flatconfig) } <$ifh>;
+    close($ifh) or warn("[WARNING] Failed to close file $path: $!");
+    close($ofh) or warn("[WARNING] Failed to close file $path, $!");
+  }
 }
 
 sub buildconf {
-    my ($flatconfig, $tdir, $bdir) = @_;
-    return sub {
-        build($flatconfig, $tdir, $bdir, $_[0]);
-    }
+  my ($flatconfig, $tdir, $bdir) = @_;
+  return sub {
+    build($flatconfig, $tdir, $bdir, $_[0]);
+  }
 }
 
 sub runinit {
-    my $path = $_[0];
-    print "[INFO] Running $path\n";
-    system("bash", $path);
+  my $path = $_[0];
+  print "[INFO] Running $path\n";
+  system("bash", $path);
 }
 
 our $filepath   = $ARGV[0];
@@ -112,9 +111,9 @@ our %flatconfig = flatten($config);
 my $mode = shift @ARGV // '';
 
 if ($mode eq 'init') {
-    print chr(7);
-    print "[PHASE] Configuring OS\n";
-    dirwalk("$scriptdir/init", \&runinit, 1);
+  print chr(7);
+  print "[PHASE] Configuring OS\n";
+  dirwalk("$scriptdir/init", \&runinit, 1);
 }
 print "[PHASE] Building dotfiles\n";
 rmtree("$scriptdir/build");
