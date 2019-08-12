5af3641645a50d060cee2ae4dea2ad39b906884c
Mariusz Litwin <litwin@oposec.pl>
2019-08-12 08:58:28 +0200

    [i3+mate] Add runner script for starting apps

---
+++ template/run-on-demand/runner.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# runner.sh : script for searching a proper alias to run based on argument
+#
+# Ref.: dotfiles/template/run-on-demand/runner.sh
+
+# Usage: bash runner.sh <prompt>
+
+if [ -f ~/.bash_aliases ]; then source ~/.bash_aliases; fi
+if [ -f ~/.bash_snowflake ]; then source ~/.bash_snowflake; fi
+
+app=$1; shift
+alias=$(alias -p | cut -c 7- | grep $app | awk -F = '{ print $2 }' | tr -d "'")
+echo $alias
+
+$($alias)
